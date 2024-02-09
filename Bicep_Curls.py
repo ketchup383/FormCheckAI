593f1cd8470873e109b7e343d29c15df1009616a
Victor Taing <153879417+vtaing10@users.noreply.github.com>
2024-02-09 00:06:59 -0500

    Official ConUHacks VIII

--- Bicep_Curls.py
+++ Bicep_Curls.py
@@ -9,6 +9,7 @@ def calculate_angle(a,b,c):
     b = np.array(b) # Mid
     c = np.array(c) # End
     
+    
     radians = np.arctan2(c[1]-b[1], c[0]-b[0]) - np.arctan2(a[1]-b[1], a[0]-b[0])
     angle = np.abs(radians*180.0/np.pi)
     
@@ -26,16 +27,16 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
     while cap.isOpened():
         ret, frame = cap.read()
         
-         # Recolor image to RGB
+        # Recolor image to RGB
         image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         image.flags.writeable = False
-      
+        
         # Make detection
         results = pose.process(image)
     
         # Recolor back to BGR
         image.flags.writeable = True
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)              
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
         
         # Extract landmarks
         try:
